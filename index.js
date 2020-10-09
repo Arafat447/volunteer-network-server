ace77f208acc8007961cdd4f2476140a12bcb0ac
Arafat447 <tahmidhasan184@gmail.com>
2020-10-09 17:45:41 +0600

    server added

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require ('cors');
+const { ObjectID, ObjectId } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://arafat447:arafatmoriom@cluster0.9a5nl.mongodb.net/volunteer?retryWrites=true&w=majority";
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("volunteer").collection("events");
+  app.post('/addVlounteer',(req,res)=>{
+      const event= req.body;
+      collection.insertOne(event)
+      .then(result => {
+        res.send(result.insertedCount>0)
+      })
+  });
+  app.get('/getEvents',(req,res)=>{
+    collection.find({email : req.query.email})
+    .toArray((err,documents)=>{
+      res.send(documents)
+    })
+  });
+
+  app.delete("/deleteEvents/:id",(req,res)=>{
+    collection.deleteOne({_id:ObjectId(req.params.id)})
+    .then(result =>{
+      res.redirect('/getEvents')
+    })
+  });
+
+  app.get('/getAllUser',(req,res)=>{
+    collection.find({})
+    .toArray((err,documents)=>{
+      res.send(documents);
+    })
+  });
+
+  app.delete("/removeEvents/:id",(req,res)=>{
+    collection.deleteOne({_id:ObjectId(req.params.id)})
+    .then(result =>{
+      res.redirect('/admin')
+    })
+  });
+ 
+});
+
+
+app.listen(4000, () => {
+  console.log('listening port 4000')
+})
